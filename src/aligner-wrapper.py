f4c85efc8d3b73206ca7678b6765515fb151f453
Alexander Okrucký <alex.okrucky@gmail.com>
2025-04-24 17:11:01 +0200

    y

--- src/aligner-wrapper.py
+++ src/aligner-wrapper.py
@@ -2,3 +2,14 @@
 
 
 !python3.12 /home/alex/Dokumenty/NeMo/tools/nemo_forced_aligner/align.py pretrained_name="stt_en_fastconformer_hybrid_large_pc" manifest_filepath=path_to_the_manifest output_dir=/home/alex/DataspellProjects/SpeechTextAlignment/create-manifest/totla-test/ save_output_file_formats=["ctm"]
+
+
+
+
+
+#!python3.12 /home/alex/Dokumenty/NeMo/tools/nemo_forced_aligner/align.py pretrained_name="stt_en_fastconformer_hybrid_large_pc" manifest_filepath=/home/alex/Dokumenty/NeMo/tutorials/tools/jane-test/manifest.json output_dir=/home/alex/Dokumenty/NeMo/tutorials/tools/jane-test/nfa_output/
+
+for i in range(0, 159):
+    manifest_path = "/home/alex/DataspellProjects/SpeechTextAlignment/create-manifest/extra-new-manifest/manifest-" + str(i) + ".json"
+    print("Doing ", str(i), "/155")
+    !python3.12 /home/alex/Dokumenty/NeMo/tools/nemo_forced_aligner/align.py pretrained_name="stt_en_fastconformer_hybrid_large_pc" manifest_filepath="$manifest_path" output_dir=/home/alex/DataspellProjects/SpeechTextAlignment/create-manifest/test-out/ save_output_file_formats=["ctm"]
